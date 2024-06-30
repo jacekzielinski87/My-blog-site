5b7b118684853b0e2619fc6e31359e5385b797e8
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-30 18:21:00 +0200

    Login Auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,35 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import AddPost from './pages/AddPost';
 import Login from './pages/Login';
+import { useState } from 'react';
+import {signOut} from 'firebase/auth';
+import {auth} from './Firebase-config';
+
 
 function App() {
-  
+   const [isAuth, setIsAuth] = useState(false);
+
+   
+
+   const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear()
+      setIsAuth(false)
+      window.location.pathname
+    })
+   };
 
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
         <Link to="/addpost">Add Post</Link>
+        {!isAuth ? <Link to="/login">Login</Link> : <button onClick={signUserOut}>Log out</button>}
       </nav>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='login' element={<Login/>}/>
         <Route path='addpost' element={<AddPost/>}/>
+        <Route path='login' element={<Login setIsAuth={setIsAuth}/>}/>
       </Routes>
     </Router>
   )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,32 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import {auth, provider} from '../Firebase-config';
+import {signInWithPopup} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
 
-function Login() {
+// eslint-disable-next-line react/prop-types
+function Login({setIsAuth}) {
+
+  let navigate = useNavigate();
+
+  const SignInWithGoogle = () => {
+    // eslint-disable-next-line no-unused-vars
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true)
+      setIsAuth(true);
+      navigate('/');
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div className='loginPage'>
+      <p>
+        Sign in With Google to Continue
+      </p>
+      <button className='login-with-google-btn' onClick={SignInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
   )
 }
 
